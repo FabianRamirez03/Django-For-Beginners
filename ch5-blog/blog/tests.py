219f45f593924c945f0c1e3090bc0d056239924c
FabianRamirez03 <ramirezfabian003@gmail.com>
2023-01-24 22:39:42 -0600

    Chapter 5 completed. Blog application with DetailView to create detailed individual views for each post

--- ch5-blog/blog/tests.py
+++ ch5-blog/blog/tests.py
@@ -1,3 +1,50 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
+from django.urls import reverse  # new
+
+from .models import Post
 
 # Create your tests here.
+class BlogTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = get_user_model().objects.create_user(
+            username="testuser", email="test@email.com", password="secret"
+        )
+
+        cls.post = Post.objects.create(
+            title="A great title",
+            body="A great body content!",
+            author=cls.user,
+        )
+
+    def test_post_model(self):
+        self.assertEqual(self.post.title, "A great title")
+        self.assertEqual(self.post.body, "A great body content!")
+        self.assertEqual(self.post.author.username, "testuser")
+        self.assertEqual(self.post.get_absolute_url(), "/post/1/")
+        self.assertEqual(
+            str(self.post), "A great title"
+        )  # Checks the name displayed on the admin page
+
+    def test_url_exists_at_correct_location_listview(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_url_exists_at_correct_location_detailview(self):
+        response = self.client.get("/post/1/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_post_listview(self):
+        response = self.client.get(reverse("home"))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "A great body content!")
+        self.assertTemplateUsed(response, "home.html")
+
+    def test_post_detailview(self):
+        response = self.client.get(reverse("post_detail", kwargs={"pk": self.post.pk}))
+        no_response = self.client.get("/post/100000/")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(no_response.status_code, 404)
+        self.assertContains(response, "A great title")
+        self.assertTemplateUsed(response, "post_detail.html")
