60877c6f31110f0a69bbc721e13f266ccf07b99f
FabianRamirez03 <ramirezfabian003@gmail.com>
2023-01-24 19:19:57 -0600

    Removed ch4 Submodule

---
+++ ch4-message-board/ch4-message-board_tmp/posts/tests.py
@@ -0,0 +1,25 @@
+from django.test import TestCase
+from django.urls import reverse
+
+from .models import Post
+
+# Create your tests here.
+
+
+class PostTests(TestCase):
+    @classmethod
+    def setUpTestData(cls) -> None:
+        cls.post = Post.objects.create(text="This is a Test!")
+
+    def test_model_content(self):
+        self.assertEqual(self.post.text, "This is a Test!")
+
+    def test_url_exists_at_correct_location(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_homepage(self):
+        response = self.client.get(reverse("home"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "home.html")
+        self.assertContains(response, "This is a Test!")
---
+++ ch4-message-board/ch4-message-board_tmp/posts/views.py
@@ -0,0 +1,8 @@
+# posts/views.py
+from django.views.generic import ListView
+from .models import Post
+
+# Create your views here.
+class HomePageView(ListView):
+    model = Post
+    template_name = "home.html"
